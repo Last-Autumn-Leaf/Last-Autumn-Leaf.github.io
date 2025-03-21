dec26a1bbe27e4146a301f5fbe913f0a7376e733
TheShyDev-Yoogo <yoogo.production@gmail.com>
2025-03-20 23:28:41 -0400

    update to make leetcode work on github

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,10 @@ const nextConfig = {
   },
   // assetPrefix: process.env.NODE_ENV === 'production' ? `/${GITLAB_PROJECT_NAME}` : '',
   output: 'export',
-  basePath: '',
+  // basePath: '',
+  basePath: '/last-autumn-leaf.github.io',  // The name of your repository
+  assetPrefix: '/last-autumn-leaf.github.io',  // Ensure static assets are prefixed correctly
+
 }
 
 export default nextConfig
