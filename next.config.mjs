792123583bebfd7ef9de1916d93eff2a909036bb
Carl-andre <yoogo.production@gmail.com>
2025-04-09 23:15:19 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,13 @@
 /** @type {import('next').NextConfig} */
 
 const GITLAB_PROJECT_NAME = 'carlos'
+
 const nextConfig = {
-  // reactStrictMode: true,
+  
+  productionBrowserSourceMaps: false,
   images: {
-    unoptimized: true,
+    unoptimized: true, 
   },
-  // assetPrefix: process.env.NODE_ENV === 'production' ? `/${GITLAB_PROJECT_NAME}` : '',
-  output: 'export',
-  // basePath: '',
-  // basePath: '/last-autumn-leaf.github.io',  // The name of your repository
 }
 
 export default nextConfig
